0e6a448025f8a219fbd69ab49d4f72850efe0d16
clumsyCoder00 <nuthanael.watkins@gmail.com>
2019-12-22 19:50:25 -0500

    Update to climate.py for HASS 0.103.0+
    Cleaned up code of outdated methods. Added precision configuration parameter, revised integration to more closely match the Schluter thermostat app.

--- climate.py
+++ climate.py
@@ -10,154 +10,195 @@ http://dev-docs.home-assistant.io/en/master/api/helpers.html
 
 todo
 - add test for token expiration
-- move schluter functions to separate file
-- make async
-- implement aiohttp
-- make object oriented
 - account for multiple thermostats
 """
 import requests
 import json
 
-#import asyncio
 import logging
 
 import voluptuous as vol
 
-#from homeassistant.core import callback
-from homeassistant.components.climate import ClimateDevice, PLATFORM_SCHEMA
+from homeassistant.components.climate import PLATFORM_SCHEMA, ClimateDevice
 from homeassistant.components.climate.const import (
     DOMAIN,
-    STATE_HEAT,
-    STATE_IDLE,
-    STATE_AUTO,
-    STATE_MANUAL,
+    ATTR_PRESET_MODE,
+    CURRENT_HVAC_HEAT,
+    CURRENT_HVAC_IDLE,
+    HVAC_MODE_HEAT,
+    HVAC_MODE_AUTO,
+    HVAC_MODE_OFF,
+    PRESET_AWAY,
+    SUPPORT_PRESET_MODE,
     SUPPORT_TARGET_TEMPERATURE,
-    SUPPORT_OPERATION_MODE,
-    SUPPORT_AWAY_MODE,
-    SUPPORT_HOLD_MODE
     )
+    
+PRESET_MANUAL = "On Manual"
+PRESET_SCHEDULE = "On Schedule"
+CONF_PRECISION = 'precision'
+
 from homeassistant.const import (
     ATTR_TEMPERATURE, TEMP_CELSIUS, TEMP_FAHRENHEIT, PRECISION_HALVES,
     PRECISION_TENTHS, PRECISION_WHOLE)
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
 import homeassistant.helpers.config_validation as cv
-#from homeassistant.helpers.dispatcher import (async_dispatcher_connect,
-#                                              async_dispatcher_send)
 
 _LOGGER = logging.getLogger(__name__)
 
-SUPPORT_FLAGS = (
-    SUPPORT_TARGET_TEMPERATURE |
-    SUPPORT_OPERATION_MODE |
-    SUPPORT_AWAY_MODE |
-    SUPPORT_HOLD_MODE
-    )
+ATTRIBUTION = "Powered by Schluter Systems"
+
+DEFAULT_PRECISION = PRECISION_WHOLE
+
+SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE | SUPPORT_PRESET_MODE
                  
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_EMAIL): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_PRECISION, default=DEFAULT_PRECISION): vol.In(
+        [PRECISION_TENTHS, PRECISION_HALVES, PRECISION_WHOLE]),
 })
 def setup_platform(hass, config, add_entities, discovery_info=None):
-#async def async_setup_platform(hass, config, async_add_entities,
-#                                discovery_info=None):
+    _LOGGER.debug("setup_platform: begin")
     """Setup the Schluter platform."""
     email = config.get(CONF_EMAIL)
     password = config.get(CONF_PASSWORD)
+    precision = config.get(CONF_PRECISION)
     
-    add_entities([SchluterThermostat(hass, email, password)], True)
-#    async_add_entities([SchluterThermostat(hass, email, password)])
+    add_entities([SchluterThermostat(hass, email, password, precision)], True)
+    _LOGGER.debug("setup_platform: end")
  
 class SchluterThermostat(ClimateDevice):
     """Representation of a Schluter thermostat device."""
     
-    def __init__(self, hass, email, password):
+    def __init__(self, hass, email, password, precision):
         """Initialize the thermostat."""
-            
-        #async def async_handle_update():
-        #    _LOGGER.debug("async_handle_update")
-        #    await self._update()
+        _LOGGER.debug("__init__: begin")
             
         self.hass = hass
         self._email = email
         self._password = password
+        self._precision = precision
         self._support_flags = SUPPORT_FLAGS
-        self._operation_list = [STATE_IDLE, STATE_HEAT]
+        self._hvac_list = [HVAC_MODE_OFF, HVAC_MODE_HEAT, HVAC_MODE_AUTO]
+        self._preset_list = [PRESET_AWAY, PRESET_MANUAL, PRESET_SCHEDULE]
         
         # data attributes
-        self._away = None                   # RegulationMode
-        self._hold = None
         self._location = None               # Group?
         self._name = None                   # Room
         self._target_temperature = None     # ComfortTemperature
         self._temperature = None            # Temperature
         self._temperature_scale = TEMP_CELSIUS
-        self._mode = None                   # Heating
-        self._is_locked = None              # VacationEnabled
-        self._locked_temperature = None     # VacationTemperature
+        self._preset_mode = None
+        self._hvac_mode = None
         self._min_temperature = None        # MinTemp
         self._max_temperature = None        # MaxTemp
-        
+#        self._is_device_active = None
         self._session_id = None
         self._serial = None
         self._thermostat_data = None
-        
+    
         # is this a platform or thermostat instance. This should be in the platform instance?
         self._get_session_id(self._email, self._password, self._session_id)
         self._serial = self._get_thermostat_serial(self._session_id)
         self.update()
-
-        #hass.services.register(DOMAIN, 'Schluter_update', async_handle_update, PLATFORM_SCHEMA)
-                
-    #async def async_added_to_hass(self):
-    #    """Device added to hass."""
-    #    _LOGGER.warning("async_added_to_hass")
-    #    async_dispatcher_connect(self.hass, 'Schluter_update',
-    #                             self._update_callback)             
-
-    #@callback
-    #def _update_callback(self):
-    #    """Update the state."""
-    #    _LOGGER.warning("_update_callback")
-    #    self.async_schedule_update_ha_state(True)
-
-    @property
-    def supported_features(self):
-        """Return the list of supported features."""
-        return self._support_flags
-                
-    @property
-    def current_operation(self):
-        """Return the current state."""
-        return self._mode        
         
+        _LOGGER.debug("__init__: end")
+
+    @property
+    def attribution(self):
+        """Return the attribution."""
+        return ATTRIBUTION
+                
     @property
     def name(self):
         """Return the name of the thermostat."""
         return self._name
-        
+
+    @property
+    def target_temperature_step(self):
+        """Return the supported step of target temperature."""
+        return self._precision
+                
     @property
     def precision(self):
         """Return the precision of the system."""
-        # PRECISION_WHOLE
-        # PRECISION_TENTHS
-        return PRECISION_TENTHS
-
+        return self._precision
+        
     @property
     def temperature_unit(self):
         """Return the unit of measurement."""
         return self._temperature_scale
-        
+                
     @property
     def current_temperature(self):
         """Return the current temperature."""
         return self._temperature
-                
+
+    @property
+    def preset_mode(self):
+        """Return current preset mode."""
+        return self._preset_mode
+        
+    @property
+    def preset_modes(self):
+        """List of available operation modes."""
+        return self._preset_list
+
+    def set_preset_mode(self, preset_mode):
+        """Set new target preset mode."""
+        if preset_mode == self.preset_mode:
+            return
+        if preset_mode == PRESET_SCHEDULE:
+            self._preset_mode = PRESET_SCHEDULE
+            self._set_termostat_data(self._session_id, self._serial, {
+                'RegulationMode': 1, # set to schedule mode
+                'VacationEnabled': False})
+        elif preset_mode == PRESET_MANUAL:
+            self._preset_mode = PRESET_MANUAL
+            self._set_termostat_data(self._session_id, self._serial, {
+                'RegulationMode': 2, # set to manual mode
+                'VacationEnabled': False})
+        elif preset_mode == PRESET_AWAY:
+            self._preset_mode = PRESET_AWAY
+            self._set_termostat_data(self._session_id, self._serial, {
+                'RegulationMode': 3, # set to away mode
+                'VacationEnabled': False})
+
     @property
     def target_temperature(self):
         """Return the temperature we try to reach."""
         return self._target_temperature
 
+    @property
+    def hvac_mode(self):
+        """Return the current state."""
+        return self._hvac_mode   
+
+    @property
+    def hvac_modes(self):
+        """List of available operation modes."""
+        return self._hvac_list
+
+    def set_hvac_mode(self, hvac_mode):
+        """Set new target hvac mode."""
+        if hvac_mode == self.hvac_mode:
+            return
+        if hvac_mode == HVAC_MODE_AUTO:
+            self._hvac_mode = HVAC_MODE_AUTO
+            self._set_termostat_data(self._session_id, self._serial, {
+                'RegulationMode': 1, # set to schedule mode
+                'VacationEnabled': False})
+        elif hvac_mode == HVAC_MODE_HEAT:
+            self._hvac_mode = HVAC_MODE_HEAT
+            self._set_termostat_data(self._session_id, self._serial, {
+                'ComfortTemperature': str(int(round(self._temperature*100,0))),
+                'RegulationMode': 2})
+        elif hvac_mode == HVAC_MODE_OFF:
+            self._hvac_mode = HVAC_MODE_OFF
+            self._set_termostat_data(self._session_id, self._serial, {
+                'RegulationMode': 3, # set to away mode
+                'VacationEnabled': False})
+        
     def set_temperature(self, **kwargs):
         """Set new target temperature."""
         temperature = kwargs.get(ATTR_TEMPERATURE)
@@ -167,17 +208,7 @@ class SchluterThermostat(ClimateDevice):
         self._set_termostat_data(self._session_id, self._serial, {
             'ComfortTemperature': str(int(round(temperature*100,0))),
             'RegulationMode': 2})
-
-    @property
-    def operation_list(self):
-        """List of available operation modes."""
-        return [STATE_HEAT, STATE_IDLE, STATE_AUTO]
-
-    def set_operation_mode(self, operation_mode):
-        """Set operation mode."""
-        if operation_mode == STATE_AUTO:
-            self._set_termostat_data(self._session_id, self._serial, {'RegulationMode': 1})
-
+        
     @property
     def min_temp(self):
         """Identify min_temp in Nest API or defaults if not available."""
@@ -187,27 +218,6 @@ class SchluterThermostat(ClimateDevice):
     def max_temp(self):
         """Identify max_temp in Nest API or defaults if not available."""
         return self._max_temperature
-
-    @property
-    def is_away_mode_on(self):
-        """Return true if away mode is on."""
-        return self._away
-        
-    # should probably do this through the group/change url the way that the webpage does
-    def turn_away_mode_on(self):
-        """Turn away mode on."""
-        self._set_termostat_data(self._session_id, self._serial, {
-            'ManualTemperature': str(int(round(self._min_temperature*100,0))),
-            'RegulationMode': 3})
-
-    def turn_away_mode_off(self):
-        """Turn away mode off."""
-        self._set_termostat_data(self._session_id, self._serial, {'RegulationMode': 1})
-                                                
-    @property
-    def current_hold_mode(self):
-        """Return the current hold mode, e.g., home, away, temp."""
-        return self._hold
         
     def _get_session_id(self, email, password, session_id):
         """Get Session ID"""
@@ -216,7 +226,7 @@ class SchluterThermostat(ClimateDevice):
         result = requests.get(request_url, params=params)
         
         if result.ok == False:
-            _LOGGER.warning("renewing session_id")
+            _LOGGER.info("renewing session_id")
             login_url = 'https://ditra-heat-e-wifi.schluter.com/api/authenticate/user'
             authenticate_payload = {
               "Email":email,
@@ -236,10 +246,22 @@ class SchluterThermostat(ClimateDevice):
     def _get_thermostat_data(self, session_id, serialnumber):
         request_url = 'https://ditra-heat-e-wifi.schluter.com/api/thermostat'
         params = {'sessionid': session_id, 'serialnumber': serialnumber}
-        return requests.get(request_url, params=params)  
+        self._thermostat_data = requests.get(request_url, params=params)
+        _LOGGER.debug("\nget_thermostat_data" + 
+            "\n\tsession_id: " + session_id + 
+            '\n\tserialnumber: ' + serialnumber + 
+            '\n\trequest_payload:\n' + 
+#            json.dumps(self._thermostat_data.json(), indent=4, sort_keys=True, skipkeys=True)
+            '\n'.join("\t\t{!r}: {!r},".format(k, v) for k, v in self._thermostat_data.json().items() if k is not "'Schedules'")
+            )
+        return self._thermostat_data.ok 
 
     # ---- set data of specific thermostat ----    
     def _set_termostat_data(self, session_id, serialnumber, request_payload):
+        _LOGGER.debug("\nset_thermostat_data" + 
+            "\n\tsession_id: " + session_id + 
+            '\n\tserailnumber: ' + serialnumber + 
+            '\n\trequest_payload:\n' + "\n".join("\t\t{!r}: {!r},".format(k, v) for k, v in request_payload.items()))
         request_url = 'https://ditra-heat-e-wifi.schluter.com/api/thermostat'
         params = {'sessionid': session_id, 'serialnumber': serialnumber}
         result = requests.post(request_url,
@@ -249,24 +271,66 @@ class SchluterThermostat(ClimateDevice):
       
     def update(self):
         self._get_session_id(self._email, self._password, self._session_id)
-        self._thermostat_data = self._get_thermostat_data(self._session_id, self._serial)
-        self._name = self._thermostat_data.json()['Room']
-        self._temperature = self._thermostat_data.json()['Temperature'] / 100
-        self._target_temperature = self._thermostat_data.json()['SetPointTemp'] / 100
-        self._min_temperature = self._thermostat_data.json()['MinTemp'] / 100
-        self._max_temperature = self._thermostat_data.json()['MaxTemp'] / 100
-        self._away = self._thermostat_data.json()['RegulationMode'] == 3
         
-        if self._thermostat_data.json()['Heating'] == False:
-            self._mode = STATE_IDLE
-        else:
-            self._mode = STATE_HEAT
+        if self._get_thermostat_data(self._session_id, self._serial):
+            self._name = self._thermostat_data.json()['Room']
+        
+            raw_temperature = self._thermostat_data.json()['Temperature']
+            if type(raw_temperature) is int:
+                self._temperature = raw_temperature / 100
+            elif raw_temperature is None:
+                _LOGGER.warning("invalid raw_temperature type: None")
+            else:
+                _LOGGER.warning("invalid raw_temperature type: " + str(type(raw_temperature)))
+            
+            self._target_temperature = self._thermostat_data.json()['SetPointTemp'] / 100
+            self._min_temperature = self._thermostat_data.json()['MinTemp'] / 100
+            self._max_temperature = self._thermostat_data.json()['MaxTemp'] / 100
 
-        if self._thermostat_data.json()['RegulationMode'] == 1:
-            self._hold = STATE_AUTO
+            if self._thermostat_data.json()['RegulationMode'] == 1:
+               self._preset_mode = PRESET_SCHEDULE
+               self._hvac_mode = HVAC_MODE_AUTO
+            elif self._thermostat_data.json()['RegulationMode'] == 2:
+               self._preset_mode = PRESET_MANUAL
+               self._hvac_mode = HVAC_MODE_HEAT
+            elif self._thermostat_data.json()['RegulationMode'] == 3:
+               self._preset_mode = PRESET_AWAY
+               self._hvac_mode = HVAC_MODE_OFF
+
+            if self._thermostat_data.json()['Heating'] == False:
+#                self._is_device_active = False
+                self._is_heating = False
+            else:
+#                self._is_device_active = True
+                self._is_heating = True
         else:
-            self._hold = STATE_MANUAL
+            _LOGGER.warning("could not get thermostat data")
+
+    @property
+    def device_info(self):
+        """Return device info for this device."""
+        return {
+            "name": self._name,
+            "serial": self._serial,
+            "manufacturer": "Schluter Systems",
+            "model": "DITRA-HEAT-E-WiFi Thermostat",
+        }
         
-#    async def async_get_something(self):
-#        await self.hass.async_add_executor_job(
-#                    self.leaf.get_latest_hvac_status)
+    @property
+    def hvac_action(self):
+        """Return the current running hvac operation if supported.
+        Need to be one of CURRENT_HVAC_*.
+        """
+        if not self._is_heating:
+            return CURRENT_HVAC_IDLE
+        return CURRENT_HVAC_HEAT
+        
+    @property
+    def _is_device_active(self):
+        """If the toggleable device is currently active."""
+        return self._is_heating
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return self._support_flags
